c852e6e4b167c8f608934b0ddc3312e2f313c609
Yasmin Velasco <ylvelasco12@ucatolica.edu.co>
2025-10-27 23:15:56 -0500

    Se configuro get_session en db.py

--- db.py
+++ db.py
@@ -1,8 +1,11 @@
-# db.py
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = "sqlite:///database.db"
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 def create_all_tables():
     SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
