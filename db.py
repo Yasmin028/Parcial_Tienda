963085b00b1ccc1454bcbe50a74a8d3d243ac786
Yasmin028 <Ylvelasco12@ucatolica.edu.co>
2025-10-28 23:37:49 -0500

    Arreglado

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = "sqlite:///database.db"
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=True)
 
-def create_all_tables():
+def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 def get_session():
