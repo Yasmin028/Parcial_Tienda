194f44b245e502499f07455f99ceb638d1de9eb8
Yasmin028 <Ylvelasco12@ucatolica.edu.co>
2025-10-28 23:41:28 -0500

    Implementacion de validaciones Pydantic en modelos de entrada y salida

--- schemas.py
+++ schemas.py
@@ -1,35 +1,27 @@
 from sqlmodel import SQLModel
 from typing import Optional
 
-class CategoriaCreate(SQLModel):
+# ===== Categorías =====
+class CategoriaBase(SQLModel):
     nombre: str
     descripcion: Optional[str] = None
 
-class CategoriaRead(SQLModel):
-    id: int
-    nombre: str
-    descripcion: Optional[str]
-    activa: bool
+class CategoriaCreate(CategoriaBase):
+    pass
 
-class ProductoCreate(SQLModel):
+class CategoriaRead(CategoriaBase):
+    id: int
+
+# ===== Productos =====
+class ProductoBase(SQLModel):
     nombre: str
     precio: float
     stock: int
-    descripcion: Optional[str] = None
-    categoria_id: int
-
-class ProductoUpdate(SQLModel):
-    nombre: Optional[str] = None
-    precio: Optional[float] = None
-    stock: Optional[int] = None
-    descripcion: Optional[str] = None
-    activo: Optional[bool] = None
+    disponible: bool = True
     categoria_id: Optional[int] = None
 
-class ProductoRead(SQLModel):
+class ProductoCreate(ProductoBase):
+    pass
+
+class ProductoRead(ProductoBase):
     id: int
-    nombre: str
-    precio: float
-    stock: int
-    descripcion: Optional[str]
-    activo: bool
